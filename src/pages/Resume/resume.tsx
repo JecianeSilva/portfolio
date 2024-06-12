2e6c79381e4afa620f6a71e1bcb4009aa662f1ce
Jeciane Silva <jeciane_amorim@hotmail.com>
2024-06-12 17:35:04 -0300

    feat: add animation particle

--- src/pages/Resume/resume.tsx
+++ src/pages/Resume/resume.tsx
@@ -1,3 +1,13 @@
+"use client";
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import {
+  type Container,
+  type ISourceOptions,
+  MoveDirection,
+  OutMode,
+} from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim";
 import { SocialMedia } from "../../components/SocialMedia/SocialMedia"
 
 import backgroundImage from "../../assets/imagens/bg-resume.webp"
@@ -5,6 +15,92 @@ import backgroundImage from "../../assets/imagens/bg-resume.webp"
 interface ResumeProps { }
 
 export function Resume({ ...rest }: ResumeProps) {
+  const [initAnimation, setInitAnimation] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInitAnimation(true);
+    });
+  }, []);
+
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
+
+  const options: ISourceOptions = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "rgba(7, 0, 28, 0.1)",
+        },
+      },
+      fpsLimit: 150,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      fullScreen: false,
+      particles: {
+        color: {
+          value: "#ffffff",
+        },
+        links: {
+          color: "#ffffff",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: MoveDirection.none,
+          enable: true,
+          outModes: {
+            default: OutMode.out,
+          },
+          random: false,
+          speed: 6,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 80,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 4 },
+        },
+      },
+      detectRetina: true,
+    }),
+    [],
+  );
+
   return (
     <section
       id="#home"
@@ -16,6 +112,13 @@ export function Resume({ ...rest }: ResumeProps) {
         height: "740px",
       }}
     >
+      {initAnimation &&
+        <Particles
+          id="tsparticles"
+          particlesLoaded={particlesLoaded}
+          options={options}
+          className="w-full h-full"
+        />}
       <div
         className="absolute bottom-0 left-0 right-0 top-0 h-full w-full flex justify-center overflow-hidden p-8">
         <div className="flex h-full w-full max-w-content items-center max-w-[1180px] pb-16">
